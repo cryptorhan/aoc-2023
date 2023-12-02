8e5209a8feef47454e7046c98d51a0ce3a99c245
cryptorhan <orhan.ors@cryptobooks.tax>
2023-12-02 14:16:09 +0300

    finalize day2 puzzle2

---
+++ 02/p2/index.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+
+const total = {
+    red: 12,
+    green: 13,
+    blue: 14
+}
+
+const processLine = (line) => {
+    const [_, gameInfo] = line.split(":")
+    let initial = {
+        green:0,
+        blue: 0,
+        red: 0 
+    }
+    for(const set of gameInfo.split(";")){
+        const cubes = set.split(",")
+        for(const c of cubes){
+            const [amount, color] = c.trimStart().split(" ")
+            if(initial[color] < parseInt(amount)){
+                initial[color] = parseInt(amount)
+            }
+        }
+    }
+    return initial.green * initial.blue * initial.red
+}
+
+const readFileLineByLine = (filename) => {
+  return new Promise((resolve, reject) => {
+    const fileStream = fs.createReadStream(filename);
+    const rl = require('readline').createInterface({
+      input: fileStream,
+      crlfDelay: Infinity
+    });
+    let totalGame = 0
+    rl.on('line', (line) => {
+        totalGame += processLine(line)
+    });
+
+    rl.on('close', () => {
+      resolve(totalGame);
+    });
+
+    rl.on('error', (err) => {
+      reject(err);
+    });
+  });
+};
+
+const main = async () => {
+  const inputFile = `${__dirname}/../input.txt`;
+  try {
+    const calibrationSum = await readFileLineByLine(inputFile);
+    console.log('Sum of calibration values:', calibrationSum);
+  } catch (err) {
+    console.error('Error reading file:', err);
+  }
+};
+
+main();
